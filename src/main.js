6547d978cd0c142af7f1bdc5d73fe6194aa2290f
Mikhail Che <mche@cpan.org>
2020-07-16 09:27:55 +0500

    дальше

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'sweet Dolly'
-	}
+  target: document.body,
+  props: {
+    name: 'sweet Dolly'
+  }
 });
 
 export default app;
